1810e1a0b29b40a015d308d3bca63f5e1d99bdb3
riyacharora <22rcharora@pinewood.edu>
2021-02-24 13:41:18 -0800

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Riya Charora on 2/24/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/githubAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubAppApp.swift
+//  Shared
+//
+//  Created by Riya Charora on 2/24/21.
+//
+
+import SwiftUI
+
+@main
+struct githubAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
